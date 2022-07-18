5d9775d879f25e9789812277db7f09b267a047d4
Caio Malassise <108196408+cmalassise@users.noreply.github.com>
2022-07-18 12:31:09 -0300

    Add files via upload

---
+++ desafios 1 e 2.ts
@@ -0,0 +1,39 @@
+let employee = {
+    code: 77,
+    name: 'Mario'
+};
+
+employee.code = 10;
+employee.name = "John";
+
+interface Person {
+    nome: string,
+    idade: number,
+    profissao: string
+}
+
+let pessoa1: Person = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz",
+};
+
+
+let pessoa2: Person = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "Padeiro",
+}
+
+
+let pessoa3: Person = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
